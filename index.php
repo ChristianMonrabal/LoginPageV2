fdd2cc646def7495a97264feb034dc96bcac6116
Christian Monrabal <90915794+ChristianMonrabal@users.noreply.github.com>
2023-06-11 12:52:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
+    rel='stylesheet'>
+    <box-icon type='solid' name='log-in'></box-icon>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Login</title>
+    <link rel="shortcut icon" href="./sources/favicon.png" type="image/x-icon">
+</head>
+<body>
+    <div class="circle"></div>
+        <h2>Accede a tu cuenta</h2>
+        <form action="" class="form">
+            <input type="email" placeholder="Email" required>
+            <input type="password" placeholder="Contraseña" required>
+            <button type="submit">Acceder</button>
+        </form>
+        <footer>
+            No tienes cuenta, registrate
+            <a href="./registrar.php">Aquí</a>
+        </footer>
+</body>
+</html>
---
+++ registrar.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' 
+    rel='stylesheet'>
+    <box-icon type='solid' name='log-in'></box-icon>
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="shortcut icon" href="./sources/favicon.png" type="image/x-icon">
+    <title>Login</title>
+</head>
+<body>
+    <div class="circle"></div>
+        <h2>Crea tu cuenta</h2>
+        <form action="" class="form">
+            <input type="email" name="mail" id="mail" placeholder="Email" required>
+            <input type="password" name="pwd" id="pwd" placeholder="Contraseña" required>
+            <button type="submit">Crear cuenta</button>
+        </form>
+        <footer>
+            Ya tienes tu cuenta, inicia sesión
+            <a href="./index.php">Aquí</a>
+        </footer>
+</body>
+</html>
